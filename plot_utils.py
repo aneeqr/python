a68fcb12a1058a29519d8f2f4dac55efb08be316
Joao Couto <jpcouto@gmail.com>
2014-06-15 17:08:22 +0200

    arg_find_peaks and cleanup

--- plot_utils.py
+++ plot_utils.py
@@ -7,18 +7,129 @@ This module contains utilities for:
     - automatic caption generation and printing.
 Requires matplotlib.pylab
 '''
+# fix the ticks direction
+from matplotlib import rcParams
+rcParams['xtick.direction'] = 'out'
+rcParams['ytick.direction'] = 'out'
 
+import os
 import matplotlib.pylab as plt
 import numpy as np
+# For printFigureWithCaption
+import subprocess
+import shlex
+import shutil
 
-__all__ = ['plotRastergram']
+__all__ = ['plotRastergram','printFigureWithCaption','fixAxes']
 
-def plotRastergram(spiketrains,offset=0,color=[0.1,0.1,0.1]):
+def plotRastergram(spiketrains,offset=0,value=None,color=[0.1,0.1,0.1]):
     '''
     Plots a rastergram of the spike trains
     Spiketrains should be a list of spiketrains
     '''
-    for i,sp in enumerate(spiketrains):
-        X = np.tile(sp,(2,1))
-        Y = np.transpose(np.tile(np.transpose([i,i+1]),(len(sp),1))+offset)
-        plt.plot(X,Y,color=color,lw=1)
+    if value is None:
+        for i,sp in enumerate(spiketrains):
+            X = np.tile(sp,(2,1))
+            Y = np.transpose(np.tile(np.transpose([i,i+0.8]),(len(sp),1))+offset)
+            plt.plot(X,Y,color=color,lw=0.6)
+    else:
+        mindist = np.min(np.diff(value))
+        for i,sp in enumerate(spiketrains):
+            X = np.tile(sp,(2,1))
+            Y = np.transpose(np.tile(np.transpose([value[i],value[i]+mindist]),(len(sp),1))+offset)
+            plt.plot(X,Y,color=color,lw=0.7)
+
+def printFigureWithCaption(filename,caption,fig=None,savefig_args={}, hide_output=True):
+    '''Uses pylab.savefig to save the file and appends a caption using latex.
+    Requires a working installation of pdflatex.
+    WARNING: Files are overwritten without prompt.
+    '''
+    if fig is None:
+        fig = plt.gcf()
+    if not 'format' in savefig_args.keys():
+        savefig_args['format']='pdf'
+    TMPFIGNAME = 'tmpFig.'+savefig_args['format']
+    TMPLATEXNAME = 'tmpLatex'
+    latex_code = r'''\documentclass{article}
+\usepackage{amsmath}
+\usepackage[active,tightpage,textmath,displaymath,floats,graphics,previewborder=0.05cm]{preview}
+\usepackage{graphicx}
+\begin{document}
+    \begin{figure}
+        \centering
+        \includegraphics[width=\textwidth]{FILENAME}
+        \caption{CAPTION}
+    \end{figure}
+\end{document}'''
+    latex_code = latex_code.replace('FILENAME',TMPFIGNAME)
+    latex_code = latex_code.replace('CAPTION',caption)
+    with open(TMPLATEXNAME+'.tex','w') as f:
+        f.write(latex_code)
+        f.close()
+    fig.savefig(TMPFIGNAME, **savefig_args)
+    # plt.show() # for debuging
+    # Use latexpdf to append caption
+    if os.path.isfile(TMPFIGNAME):
+        stdout = None
+        if hide_output:
+            stdout = open('/dev/null', 'w')
+        proc = subprocess.Popen(shlex.split('pdflatex -interaction=nonstopmode '
+                                            +TMPLATEXNAME+'.tex'),stdout=stdout)
+        proc.wait()
+        # Remove unwanted files and move to requested location
+        os.unlink(TMPLATEXNAME+'.tex')   
+        os.unlink(TMPLATEXNAME+'.aux')   
+        os.unlink(TMPLATEXNAME+'.log')
+        shutil.move(TMPLATEXNAME+'.pdf',filename)
+        os.unlink(TMPFIGNAME)   
+    else:
+        print('Figure not saved...HELP...')
+        plt.show()
+def fixAxes(ax=None,xlabel='',ylabel='',xloc='bottom',yloc='left',xposition=('outward',1.5),yposition=('outward',1.5),lw=0.7,fontsize=8,xcolor='black',ycolor='black'):
+    notxloc='bottom'
+    notyloc='right'
+    if xloc=='bottom':
+        notxloc='top'
+    if yloc=='right':
+        notyloc='left'
+    if ax==None:
+        ax=plt.gca()
+    ax.set_axisbelow(True)
+    ax.spines[notxloc].set_visible(False)
+    ax.spines[notyloc].set_visible(False)
+    ax.spines[xloc].set_visible(True)
+    ax.spines[yloc].set_visible(True)
+    ax.spines[xloc].set_lw(lw)
+    ax.spines[xloc].set_color(xcolor)
+    ax.spines[xloc].set_position(xposition)
+    ax.spines[yloc].set_lw(lw)
+    ax.spines[yloc].set_color(ycolor)
+    ax.spines[yloc].set_position(yposition)
+    ax.yaxis.set_ticks_position(yloc)
+    ax.xaxis.set_ticks_position(xloc)
+    xax = ax.xaxis
+    xax.set_label_text(xlabel)
+    xax.set_label_position(xloc)
+    xax.set_label_position(xloc)
+    xax.label.set_color(xcolor)
+    xax.label.set_size(fontsize)
+    yax = ax.yaxis
+    yax.set_label_text(ylabel)
+    yax.set_label_position(yloc)
+    yax.set_label_position(yloc)
+    yax.label.set_color(ycolor)
+    yax.label.set_size(fontsize)
+    for label in ax.get_xticklabels():
+            label.set_color(xcolor)
+            label.set_fontsize(fontsize)
+    for label in ax.get_yticklabels():
+            label.set_color(ycolor)
+            label.set_fontsize(fontsize)
+    for label in ax.get_xticklines():
+            label.set_color(xcolor)
+            label.set_lw(lw)
+            label.set_markersize(4)
+    for label in ax.get_yticklines():
+            label.set_color(ycolor)
+            label.set_lw(lw)
+            label.set_markersize(4)
--- spk_utils.py
+++ spk_utils.py
@@ -9,8 +9,9 @@ Joao Couto - 17 Jan 2013
 
 import numpy as np
 from scipy.signal import find_peaks_cwt
+import pylab as plt
 
-__all__=['entropy']
+__all__=['entropy','argfindpeaks']
 
 def _calculate_edges_with_fixed_resolution(minedge, maxedge, res=20, 
                                      scale='log', verbose=False):
@@ -21,42 +22,88 @@ def _calculate_edges_with_fixed_resolution(minedge, maxedge, res=20,
     res = float(res)
     if scale == 'log':
         K = res*np.log10(maxedge/float(minedge))
-        print K
-        
         if verbose:
             print('Returning log scale edges (base 10).')
-        print np.arange(1,K+1)
         return float(minedge)*10**(np.arange(1,K+1)/res)
     else:
         if verbose:
             print('Returning linear scale edges.')
         return np.arange(minedge,maxedge,res)
-    
-def entropy(spiketrain, edges=None, res=20, scale='log'):
+
+def _calculate_edges_for_entropy_map(minimum=0.4,maximum=250,parameter=20):
+    ''' Calculates the edges using trial and error and with fixed limits.
+    '''
+    edges = [minimum]
+    k = 1
+    while edges[len(edges)-1]<maximum+1:
+        edges.extend([round(minimum*10**(float(k)/parameter),2)]) #adjust parameter....
+        k+=1
+        if k>1000:
+            print "too many edges......k above 1000... restarting with diferent parameter..."
+            k=1
+            edges=[minimum]
+            parameter-=3
+            if parameter<0:
+                parameter=1
+                print "---> We have a problem...."
+    return np.array(edges)
+
+def entropy(spiketrain,edges=None, res=20, scale='log',order=2,input_switch = 'spiketrain',plot=False):
     ''' Calculates the entropy of a spiketrain.
     For reference see Dorval et al. 2009.
     Order [2] defines the order to use. 
     '''
-    isi = np.diff(spiketrain)
-    if not len(isi):
+    if input_switch in ['spiketrain']:
+        isi = np.diff(spiketrain)
+    else:
+        isi = spiketrain
+    if not len(isi)>1:
         # There is no entropy on an empty spiketrain.
         # NOTE: It is zero by definition.
         return np.nan
     else:
         if edges is None:
             # Calculate edges
-            edges = _calculate_edges_with_fixed_resolution(np.min(isi)*0.8, 
-                                                     np.max(isi)*1.2,
-                                                     res,scale)
+            # Use limits 0.4 250 in analysing MRG otherwise let the axes be given by the dataset
+            edges = _calculate_edges_with_fixed_resolution(0.4,250,res,scale)
+                                                           #np.min(isi)*0.8, 
+                                                           #np.max(isi)*1.2,
+                                                           #res,scale)
+#            edges = _calculate_edges_for_entropy_map(0.4,250,parameter=20)
         # In Dorval et al. 2009 the probabity is defined 
         # as the # of isis in each bin divided by the number of events,
         # so we can not use the density.
-        counts,edges = np.histogram(isi,edges,density=False)
-        Pisi = counts/float(len(isi))
-        idx = np.nonzero(Pisi)
-        H = -np.sum(Pisi[idx]*np.log2(Pisi[idx]))
-        return H
-    
+        if order == 1:
+            counts,edges = np.histogram(isi,edges,density=False)
+            Pisi = counts/float(len(isi))
+            idx = np.nonzero(Pisi)
+            return -np.sum(Pisi[idx]*np.log2(Pisi[idx]))
+        if order == 2:
+            counts,edges0,edges1 = np.histogram2d(isi[:-1],isi[1:],edges)
+            Pisi = counts/float(len(isi)-1)
+            # idx = np.nonzero(Pisi)
+
+            x,y = np.where(Pisi)
+            if plot:
+                ax =  plt.gca()
+                linedge = edges.copy()
+                for ii in range(0,len(edges0)-1):
+                    edges0[ii] = edges0[ii]+(edges0[ii+1]-edges0[ii])/2.0 
+                    edges1[ii] = edges1[ii]+(edges1[ii+1]-edges1[ii])/2.0
+                ax.scatter(np.log(edges0[x]),np.log(edges1[y]),c=Pisi[x,y], 
+                           s=counts[x,y] / np.sum(counts[x, y]),
+                           cmap=plt.cm.gray, vmax=1, vmin=0)
+                t = [1, 2, 5, 10, 50, 100, 200]
+                ax.set_yticks(np.log(t), t)
+                ax.set_xticks(np.log(t), t)
+                ax.axis([0, np.log(300), 0, np.log(300)])
+                ax.grid(1, alpha = 0.6)
+#                ax.scatter(isi[:-1],isi[1:],20,'r')
+                # ax.hlines(linedge,min(edges[::3]),max(edges[::3] ),lw=0.3)
+                # ax.vlines(linedge,min(edges[:: 3]),max(edges[:: 3]),lw=0.3)
+                return (edges, counts)
+            return -0.5*np.sum(Pisi[x,y]*np.log2(Pisi[x,y]))
+
 def argfindpeaks(data, threshold=-20,deadwindow=30):
     ''' Extracts the indexes of the peaks in the data with threshold crossing.
     Uses a dead window/minimum peak distance.
